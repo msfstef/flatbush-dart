2948b1be2b6f50bd3ecd6764e3359169fd07f538
Stefanos Mousafeiris <msfstef@gmail.com>
2023-08-01 17:34:16 +0300

    initialize index variables

--- lib/flatbush_dart.dart
+++ lib/flatbush_dart.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 /// Fast spatial index for 2D points and rectangles.
 abstract class Flatbush {
   /// Creates a new index that will hold [numItems] number of
@@ -6,10 +8,94 @@ abstract class Flatbush {
   /// The [nodeSize] parameter controls the size of the tree
   /// node ([defaultNodeSize] by default). Increasing this value
   /// makes indexing faster and queries slower, and vice versa.
-  const Flatbush({
+  Flatbush({
     required this.numItems,
     this.nodeSize = defaultNodeSize,
-  });
+    this.coordinateArrayViewConstructor = Float64List.view,
+  }) {
+    if (numItems <= 0) {
+      throw ArgumentError('Received non-positive numItems value: $numItems.');
+    }
+
+    if (nodeSize <= 2 || nodeSize >= 65535) {
+      throw ArgumentError(
+        'Received invalid nodeSize value: $nodeSize.'
+        ' Must be between 2 and 2^16 - 1.',
+      );
+    }
+
+    final coordinateArraySample = coordinateArrayViewConstructor(
+      Uint8List(0).buffer,
+    );
+    final coordinateArrayElementSizeInBytes =
+        coordinateArraySample.elementSizeInBytes;
+    final coordinateArrayType = coordinateArraySample.runtimeType;
+
+    final coordinateArrayTypeIndex = _arrayTypes.indexOf(coordinateArrayType);
+    if (coordinateArrayTypeIndex < 0) {
+      throw ArgumentError(
+        'Unexpected typed array class: $coordinateArrayType',
+      );
+    }
+
+    // calculate the total number of nodes in the R-tree to allocate space for
+    // and the index of each tree level (used in search later)
+    var n = numItems;
+
+    var numNodes = n;
+    _levelBounds.add(n * 4);
+    do {
+      n = (n / nodeSize).ceil();
+      numNodes += n;
+      _levelBounds.add(numNodes * 4);
+    } while (n != 1);
+
+    indexArrayViewConstructor = (numNodes < 2 ^ 14
+        ? Uint16List.view
+        : Uint32List.view) as TypedArrayViewConstructor;
+    final indexArrayElementSizeInBytes = indexArrayViewConstructor(
+      Uint8List(0).buffer,
+    ).elementSizeInBytes;
+
+    final nodesByteSize = numNodes * 4 * coordinateArrayElementSizeInBytes;
+
+    _data = Uint8List(
+      8 + nodesByteSize + numNodes * indexArrayElementSizeInBytes,
+    ).buffer;
+    _boxes =
+        coordinateArrayViewConstructor(_data, 8, numNodes * 4) as List<num>;
+    _indices = indexArrayViewConstructor(_data, 8 + nodesByteSize, numNodes)
+        as List<num>;
+    _pos = 0;
+    _indexMinX = double.infinity;
+    _indexMinY = double.infinity;
+    _indexMaxX = double.negativeInfinity;
+    _indexMaxY = double.negativeInfinity;
+
+    // set header information
+    Uint8List.view(_data, 0, 2).setRange(0, 2, [
+      0xfb,
+      (_version << 4) + coordinateArrayTypeIndex,
+    ]);
+    Uint16List.view(_data, 2, 1)[0] = nodeSize;
+    Uint32List.view(_data, 4, 1)[0] = numItems;
+  }
+
+  /// Allowed typed array types.
+  static const List<Type> _arrayTypes = [
+    Int8List,
+    Uint8List,
+    Uint8ClampedList,
+    Int16List,
+    Uint16List,
+    Int32List,
+    Uint32List,
+    Float32List,
+    Float64List,
+  ];
+
+  /// Serialized format version.
+  static const int _version = 3;
 
   /// Default size of the tree node.
   static const int defaultNodeSize = 16;
@@ -20,14 +106,46 @@ abstract class Flatbush {
   /// Size of the tree node of the index.
   final int nodeSize;
 
-  /// Adds a given rectangle to the index, returns
+  /// The typed array constructor used for coordinate storage.
+  final TypedArrayViewConstructor coordinateArrayViewConstructor;
+
+  /// The typed array constructor used for index storage.
+  late final TypedArrayViewConstructor indexArrayViewConstructor;
+
+  late ByteBuffer _data;
+  late List<num> _boxes;
+  late List<num> _indices;
+  List<int> _levelBounds = [];
+  int _pos = 0;
+  num _indexMinX = double.infinity;
+  num _indexMinY = double.infinity;
+  num _indexMaxX = double.negativeInfinity;
+  num _indexMaxY = double.negativeInfinity;
+  // List<int> _queue = [];
+
+  /// Adds a given rectangle to the index, specified by
+  /// [minX], [minY], [maxX], and [maxY], and returns
   /// the corresponding [ItemIdx] for the added item.
   ItemIdx add({
-    required double minX,
-    required double minY,
-    required double maxX,
-    required double maxY,
-  });
+    required num minX,
+    required num minY,
+    required num maxX,
+    required num maxY,
+  }) {
+    final index = _pos >> 2;
+    _indices[index] = index;
+    _boxes[_pos++] = minX;
+    _boxes[_pos++] = minY;
+    _boxes[_pos++] = maxX;
+    _boxes[_pos++] = maxY;
+
+    if (minX < _indexMinX) _indexMinX = minX;
+    if (minY < _indexMinY) _indexMinY = minY;
+    if (maxX > _indexMaxX) _indexMaxX = maxX;
+    if (maxY > _indexMaxY) _indexMaxY = maxY;
+
+    return index;
+  }
 
   /// Performs indexing of the added rectangles.
   /// The number of rectangles added must match the one specified
@@ -73,3 +191,13 @@ typedef ItemIdx = int;
 /// A function that filters the results of a search
 /// based on the [index] of the item.
 typedef FlatbushFilter = bool Function(ItemIdx index);
+
+/// Constructor for a typed array view of byte bufer
+typedef TypedArrayViewConstructor = TypedData Function(
+  ByteBuffer buffer, [
+  int offsetInBytes,
+  int? length,
+]);
+
+/// Constructor for data buffers
+typedef DataBufferConstructor = TypedData Function(int length);
